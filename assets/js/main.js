349a82c688b72ab3dc55559ab11eaba5c99e267d
cmartin27 <carmen.martin.moreno@hotmail.com>
2022-12-26 15:07:08 +0100

    Removed navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,7 @@
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
-    select('body').classList.toggle('mobile-nav-active')
+    //select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
